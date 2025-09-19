9ca4b8f2d348d2ec3f71216f31336488932e275c
Elin Aas <elina@met.no>
2025-09-19 07:43:53 +0000

    Transfer observed road surface temperatures

--- transfer_NORTRIP_combined_meteodata.f90
+++ transfer_NORTRIP_combined_meteodata.f90
@@ -25,28 +25,28 @@
         
 !Distribute meteo data to roadlinks. Saves all links or specified links.
         do jj=1,n_save_links
-        i=save_links(jj)
-               
-        if ((inputdata_int_rl(savedata_rl_index,i).eq.1.and.use_only_special_links_flag.ge.1) &
-            .or.(use_only_special_links_flag.eq.0).or.(use_only_special_links_flag.eq.2)) then
-        
-            do t=1,transfer_n_time
+            i=save_links(jj)
                 
-                transfer_meteo_data(transfer_T_a_index,t,jj)=meteo_output(temperature_index,t,i)
-                transfer_meteo_data(transfer_FF_index,t,jj)=meteo_output(speed_wind_index,t,i)
-                transfer_meteo_data(transfer_DD_index,t,jj)=meteo_output(dir_wind_index,t,i)
-                transfer_meteo_data(transfer_RH_index,t,jj)=meteo_output(relhumidity_index,t,i)
-                transfer_meteo_data(transfer_Rain_precip_index,t,jj)=meteo_output(rain_index,t,i)
-                transfer_meteo_data(transfer_Snow_precip_index,t,jj)=meteo_output(snow_index,t,i)
-                transfer_meteo_data(transfer_short_rad_in_index,t,jj)=meteo_output(shortwaveradiation_index,t,i)
-                transfer_meteo_data(transfer_long_rad_in_index,t,jj)=meteo_output(longwaveradiation_index,t,i)
-                transfer_meteo_data(transfer_cloud_cover_index,t,jj)=meteo_output(cloudfraction_index,t,i)
-                transfer_meteo_data(transfer_pressure_index,t,jj)=meteo_output(pressure_index,t,i)
-                transfer_meteo_data(transfer_road_temperature_obs_input_index,t,jj)=meteo_output(road_temperature_index,t,i)
+            if ((inputdata_int_rl(savedata_rl_index,i).eq.1.and.use_only_special_links_flag.ge.1) &
+                .or.(use_only_special_links_flag.eq.0).or.(use_only_special_links_flag.eq.2)) then
+            
+                do t=1,transfer_n_time
+                    
+                    transfer_meteo_data(transfer_T_a_index,t,jj)=meteo_output(temperature_index,t,i)
+                    transfer_meteo_data(transfer_FF_index,t,jj)=meteo_output(speed_wind_index,t,i)
+                    transfer_meteo_data(transfer_DD_index,t,jj)=meteo_output(dir_wind_index,t,i)
+                    transfer_meteo_data(transfer_RH_index,t,jj)=meteo_output(relhumidity_index,t,i)
+                    transfer_meteo_data(transfer_Rain_precip_index,t,jj)=meteo_output(rain_index,t,i)
+                    transfer_meteo_data(transfer_Snow_precip_index,t,jj)=meteo_output(snow_index,t,i)
+                    transfer_meteo_data(transfer_short_rad_in_index,t,jj)=meteo_output(shortwaveradiation_index,t,i)
+                    transfer_meteo_data(transfer_long_rad_in_index,t,jj)=meteo_output(longwaveradiation_index,t,i)
+                    transfer_meteo_data(transfer_cloud_cover_index,t,jj)=meteo_output(cloudfraction_index,t,i)
+                    transfer_meteo_data(transfer_pressure_index,t,jj)=meteo_output(pressure_index,t,i)
+                    transfer_meteo_data(transfer_road_temperature_obs_input_index,t,jj)=meteo_output(road_temperature_index,t,i)
 
-            enddo
+                enddo
 
-        endif
+            endif
         enddo
     
                 transfer_available_meteo_data(transfer_T_a_index)=.true.
@@ -59,7 +59,7 @@
                 transfer_available_meteo_data(transfer_long_rad_in_index)=.true.
                 transfer_available_meteo_data(transfer_cloud_cover_index)=.true.
                 transfer_available_meteo_data(transfer_pressure_index)=.true.
-                transfer_available_meteo_data(transfer_road_temperature_obs_input_index)=.false.
+                transfer_available_meteo_data(transfer_road_temperature_obs_input_index)=.true.
     
        if (allocated(meteo_output)) deallocate (meteo_output)
 
